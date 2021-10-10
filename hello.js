a13fdeadf202366bf8296750c118918a0d683ecf
Vaishali Sharma <vaishbhardwaj2000@gmail.com>
2021-10-10 17:27:39 +0530

    dataTypes

---
+++ hello.js
@@ -0,0 +1,24 @@
+console.log("Hello JavaScript")
+console.log(true)
+console.log(false)
+console.log(12)
+console.log(12.453)
+console.log("true")
+console.log("false")
+console.log("12")
+
+var d1 = true
+var d2 = false
+var d3 = 12
+var d4 = 12.453
+var d5 = "true"
+var d6 = "true"
+var d7 = "12"
+
+console.log(d1)
+console.log(d2)
+console.log(d3)
+console.log(d4)
+console.log(d5)
+console.log(d6)
+console.log(d7)
